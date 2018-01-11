065a362ac46ed38a0e30e179aa474958b773fad2
Gurjot Singh <gurjotssidhu08@gmail.com>
2018-01-11 20:42:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function destructivelyAppendKitten(name){
 }
 
 function destructivelyPrependKitten(name){
-  kittens.unshift("Bob");
+  kittens.unshift(name);
 }
 
 function destructivelyRemoveLastKitten(){
